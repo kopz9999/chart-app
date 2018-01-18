7d63499638e43ff0e7751591c6193c3edc2299a0
Luis Canales <kopz9999@gmail.com>
2018-01-18 01:29:17 -0800

    ticks setup

--- src/app/components/main-container/main-container.component.ts
+++ src/app/components/main-container/main-container.component.ts
@@ -10,7 +10,23 @@ export class MainContainerComponent implements OnInit {
   @ViewChild('currentForm') currentForm: ChartFormComponent;
   @ViewChild('desiredForm') desiredForm: ChartFormComponent;
   chartOptions = {
-    responsive: true
+    responsive: true,
+    scale: {
+      angleLines: {
+        display: false
+      },
+      ticks: {
+        beginAtZero: true,
+        max: 5,
+        min: 0,
+        stepSize: 1,
+        labels: false,
+        showLabelBackdrop: false,
+        userCallback: function (value, index, values) {
+          return '';
+        }
+      }
+    }
   };
 
   chartData = [
